cf3ddd4a2880f7440549152e4165deee5f31840c
William <byteblock6@gmail.com>
2025-05-19 22:57:20 +0800

    update ufw.sh

--- ufw.sh
+++ ufw.sh
@@ -1,4 +1,12 @@
-ufw allow 8899
-ufw allow 10800
-ufw allow 10900
-ufw allow 20000/udp
+#!/bin/bash
+
+set -e  # 脚本有报错就终止
+
+# 开启 UFW（如果没启用）
+ufw enable
+
+sudo ufw allow 22
+sudo ufw allow 8000:8020/tcp
+sudo ufw allow 8000:8020/udp
+sudo ufw allow 8899 # http 端口
+sudo ufw allow 8900 # websocket 端口
